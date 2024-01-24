dade9a24a52309269161194d3ed5a5b45ef5b481
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2024-01-24 09:50:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "18654447"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "60ac6f65c766e73dfcc1debef93d06bc"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "1225278379"
+SUPPORT_GRP = "Iink_ka_adda"
+UPDATE_CHNL = "CodeNexus_community"
+OWNER_USERNAME = "Decent_op"
 
