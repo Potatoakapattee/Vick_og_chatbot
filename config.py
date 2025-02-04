6ffb93b8e51c1feee20e8c6dc7b6f545457aeda0
Potatoakapattee <decentboy022k@gmail.com>
2025-02-04 11:13:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ API_ID = "18654447"
 # -------------------------------------------------------------
 API_HASH = "60ac6f65c766e73dfcc1debef93d06bc"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = "6592461120:AAEPOM0QY7l12L9p32rLK9w-rHWxlrjNu_w"
+MONGO_URL = "mongodb+srv://abc:abcd@cluster0.lnbu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 OWNER_ID = "1225278379"
 SUPPORT_GRP = "Iink_ka_adda"
 UPDATE_CHNL = "CodeNexus_community"
